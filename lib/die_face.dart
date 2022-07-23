092d7a0d450ce02ffa984474553e952fb599f1f2
swittman <legotech25@gmail.com>
2022-07-23 02:06:41 -0700

    Initial commit.

---
+++ lib/die_face.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+enum DieIcon { blank, shield, book, star, skull }
+
+class DieFace extends StatelessWidget {
+  const DieFace({
+    Key? key,
+    this.color = const Color(0xFF222222),
+    this.inkColor = const Color(0xFFEEEEEE),
+    this.number,
+    this.iconTL = DieIcon.blank,
+    this.iconTR = DieIcon.blank,
+    this.iconBL = DieIcon.blank,
+    this.iconBR = DieIcon.blank,
+  }) : super(key: key);
+
+  final Color color, inkColor;
+  final DieIcon iconTL, iconTR, iconBL, iconBR;
+  final int? number;
+
+  // These are divisors, so a larger number is a smaller icon.
+  static int largeIconScale = 3;
+  static int smallIconScale = 5;
+  static int iconPaddingScale = 10;
+  static int textScale = 3;
+  static int skullScale = 2;
+  static int radiusScale = 20;
+
+  IconData? _iconFromEnum(DieIcon iconEnum) {
+    switch (iconEnum) {
+      case DieIcon.shield:
+        return Icons.shield;
+      case DieIcon.book:
+        return Icons.book;
+      case DieIcon.star:
+        return Icons.star;
+      case DieIcon.skull:
+        return Icons.cancel;
+      case DieIcon.blank:
+      default:
+        return null;
+    }
+  }
+
+  Container _containerForIcon(
+      DieIcon icon, Alignment alignment, BoxConstraints constraint) {
+    return Container(
+      margin: EdgeInsets.all(constraint.maxHeight / iconPaddingScale),
+      alignment: alignment,
+      child: Icon(_iconFromEnum(icon),
+          color: inkColor,
+          size: constraint.maxHeight /
+              (number == null ? largeIconScale : smallIconScale)),
+    );
+  }
+
+  bool _hasSkull() {
+    return iconTL == DieIcon.skull ||
+        iconTR == DieIcon.skull ||
+        iconBL == DieIcon.skull ||
+        iconBR == DieIcon.skull;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1,
+      child: LayoutBuilder(
+        builder: (context, constraint) {
+          return Container(
+            decoration: BoxDecoration(
+                color: color,
+                borderRadius: BorderRadius.all(
+                    Radius.circular(constraint.maxHeight / radiusScale))),
+            child: (_hasSkull()
+                ? Container(
+                    alignment: Alignment.center,
+                    child: Icon(_iconFromEnum(DieIcon.skull),
+                        color: inkColor,
+                        size: constraint.maxHeight / skullScale),
+                  )
+                : Stack(
+                    children: <Widget>[
+                      _containerForIcon(iconTL, Alignment.topLeft, constraint),
+                      _containerForIcon(iconTR, Alignment.topRight, constraint),
+                      _containerForIcon(
+                          iconBL, Alignment.bottomLeft, constraint),
+                      _containerForIcon(
+                          iconBR, Alignment.bottomRight, constraint),
+                      if (number != null)
+                        Container(
+                          alignment: Alignment.center,
+                          child: Text(
+                            number.toString(),
+                            style: TextStyle(
+                                color: inkColor,
+                                fontSize: constraint.maxHeight / textScale),
+                          ),
+                        ),
+                    ],
+                  )),
+          );
+        },
+      ),
+    );
+  }
+}
